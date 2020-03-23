84aef63b33f05051560f0eb9b4ff4d77522b2693
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-23 23:29:47 +0100

    ADD: DialogObject

---
+++ Assets/Scripts/Dialog/DialogObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogObject
+{
+    private string type; // line or decision
+    private string dialogLine;
+    private string[] dialogDecisions;
+
+    // Getter
+    public string GetType()
+    {
+        return this.type;
+    }
+    public string GetDialogLine()
+    {
+        return this.dialogLine;
+    }
+    public string[] GetDialogDecisions()
+    {
+        return dialogDecisions;
+    }
+
+    // Setter
+    public void SetType(string type)
+    {
+        this.type = type;
+    }
+    public void SetDialogLine(string dialogLine)
+    {
+        this.dialogLine = dialogLine;
+    }
+    public void SetDialogDecisions(string[] dialogDecisions)
+    {
+        this.dialogDecisions = dialogDecisions;
+    }
+}
