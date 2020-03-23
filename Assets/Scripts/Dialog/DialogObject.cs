0bda28a6013b5012e60f4b138e6435b3025d55af
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-23 23:45:35 +0100

    REFACTOR: DialogObject

--- Assets/Scripts/Dialog/DialogObject.cs
+++ Assets/Scripts/Dialog/DialogObject.cs
@@ -4,35 +4,7 @@ using UnityEngine;
 
 public class DialogObject
 {
-    private string type; // line or decision
-    private string dialogLine;
-    private string[] dialogDecisions;
-
-    // Getter
-    public string GetType()
-    {
-        return this.type;
-    }
-    public string GetDialogLine()
-    {
-        return this.dialogLine;
-    }
-    public string[] GetDialogDecisions()
-    {
-        return dialogDecisions;
-    }
-
-    // Setter
-    public void SetType(string type)
-    {
-        this.type = type;
-    }
-    public void SetDialogLine(string dialogLine)
-    {
-        this.dialogLine = dialogLine;
-    }
-    public void SetDialogDecisions(string[] dialogDecisions)
-    {
-        this.dialogDecisions = dialogDecisions;
-    }
+    private string type { get; set; } // line or decision
+    private string dialogLine { get; set; }
+    private string[] dialogDecisions { get; set; }
 }
