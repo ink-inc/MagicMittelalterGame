e84f7ac92809682e57d0669bccf233284dbb432d
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-25 19:15:16 +0100

    Revert "Revert "Revert "Revert "Merge branch 'talkingsystem/production' into talkingsystem/development-Lukas""""
    This reverts commit 1e3248973bd57e51878eec84e4cb7e6a3de43317.

---
+++ Assets/DebugScreen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugScreen : MonoBehaviour
+{
+    public Transform coordinatesObject;
+    public Text coordsText;
+
+    public bool showCoord_x = true;
+    public bool showCoord_y = false;
+    public bool showCoord_z = true;
+    public int digitsAfterPoint = 2;
+
+    public Text fpsCounter;
+    public int avgFrameRate;
+
+    private void Start()
+    {
+        if (coordinatesObject == null)
+        {
+            Logger.log("No coordinates object for Debug screen... Looking for player controller...");
+            PlayerController[] foundController = FindObjectsOfType<PlayerController>();
+            if (foundController.Length == 1)
+            {
+                coordinatesObject = foundController[0].transform;
+                Logger.log("Controller automatically assigned. Please fix that for later builds.");
+            }
+            else
+            {
+                Logger.log("No singleton controller found!");
+            }
+        }
+    }
+
+    private void Update()
+    {
+        coordsText.text = "";
+        if (showCoord_x)
+            coordsText.text += " X:" + coordinatesObject.position.x.ToString("F" + digitsAfterPoint);
+        if (showCoord_y)
+            coordsText.text += " Y:" + coordinatesObject.position.y.ToString("F" + digitsAfterPoint);
+        if (showCoord_z)
+            coordsText.text += " Z:" + coordinatesObject.position.z.ToString("F" + digitsAfterPoint);
+
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        fpsCounter.text = "FPS: " + avgFrameRate.ToString();
+    }
+}
---
+++ Assets/Scripts/Interaction/Interactable_Healthbar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_Healthbar : Interactable
+{
+
+    public PlayerHealthbar playerHealthbar;
+    public PlayerProperties playerProp;
+    public int value;
+    public override void interact()
+    {
+        playerProp.SetHealth(Mathf.Clamp(playerProp.GetCurHealth() + value, 0, playerProp.GetMaxHealth())); //changes players current health
+        playerHealthbar.SetHealth(playerProp.GetCurHealth());       //adjusts player healthbar
+    }
+}
