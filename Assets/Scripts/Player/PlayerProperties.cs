d228ed3cc49e829b6f10c5596739f175811c699e
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-26 16:56:00 +0100

    REFACTOR: Speed values to playerproperties

--- Assets/Scripts/Player/PlayerProperties.cs
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -9,6 +9,12 @@ public class PlayerProperties : MonoBehaviour
     public float currentHealth = 100;
     public float maxHealth = 100;
 
+    [Header("Speed values")]
+    public float walkingSpeed;
+    public float runningSpeed;
+    public float sneakMultiplier;
+    public float jumpPower;
+
     public float GetCurHealth()
     {
         return currentHealth;
