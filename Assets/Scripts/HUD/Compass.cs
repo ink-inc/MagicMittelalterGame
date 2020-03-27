45b8e6b411c1cdcad5fc5bbb96aab141b9e39ce9
kosch104 <Konsicrafter@web.de>
2020-03-27 16:28:20 +0100

    FIX: Compass by adding offset

--- Assets/Scripts/HUD/Compass.cs
+++ Assets/Scripts/HUD/Compass.cs
@@ -19,6 +19,6 @@ public class Compass : MonoBehaviour
         int degrees = (int)directionParent.rotation.eulerAngles.y;
         compassText.text = degrees.ToString();
 
-        compassImage.uvRect = new Rect(directionParent.localEulerAngles.y / 360f, 0f, 0.5f, 0.5f);
+        compassImage.uvRect = new Rect(directionParent.localEulerAngles.y / 360f + 0.25f, 0f, 0.5f, 0.5f);
     }
 }
