979212664fc575f35dffead7371bc070f9a26e75
kosch104 <Konsicrafter@web.de>
2020-03-27 12:40:26 +0100

    UPDATE: PlayerHealthbar to include relative values

--- Assets/Scripts/Player/PlayerHealthbar.cs
+++ Assets/Scripts/Player/PlayerHealthbar.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 
 public class PlayerHealthbar : MonoBehaviour
 {
-
     public Image healthbarBack;
     public Image healthbarFront;
     public PlayerProperties prop;
 
-    public void SetHealth(float currentHealth)  //Adjusts red health bar to current health 
+    public void SetHealth(float currentHealth)  //Adjusts red health bar to current health
     {
         float maxHealth = prop.GetMaxHealth();
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         float maxHealthPercentage = maxHealth / 100;
         float healthbarPercentageFilled = currentHealth / maxHealthPercentage;
-        healthbarFront.rectTransform.sizeDelta = new Vector2(healthbarPercentageFilled * 2, 20);  //healthbarSize currently 200 -> 2px equals 1% health
+        float absoluteValue = healthbarPercentageFilled * (healthbarBack.rectTransform.sizeDelta.x / 100); //Calculation: Percentage * (width of parent / 100) -> width for child
+        healthbarFront.rectTransform.sizeDelta = new Vector2(absoluteValue, 20);
+    }
+
+    public void Refresh()
+    {
+        SetHealth(prop.GetCurHealth());
     }
 }
