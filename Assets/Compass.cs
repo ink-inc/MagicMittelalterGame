b1d3276c1af250068d054aae7f4af5c1adeb8477
kosch104 <Konsicrafter@web.de>
2020-03-27 13:31:18 +0100

    ADD: Compass direction script, UI and HUD-Prefab

---
+++ Assets/Compass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+    public Text compassText;
+
+    public Transform directionParent;
+
+    private void FixedUpdate()
+    {
+        updateCompass();
+    }
+
+    private void updateCompass()
+    {
+        int direction = (int)directionParent.rotation.eulerAngles.y;
+        compassText.text = "Direction: " + direction;
+    }
+}
