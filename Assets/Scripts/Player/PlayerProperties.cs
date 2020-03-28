4c72cf97514004cfc965c0a64ccefa5b87be7730
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-28 13:02:59 +0100

    UPDATE: Player Properties

--- Assets/Scripts/Player/PlayerProperties.cs
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -6,14 +6,14 @@ public class PlayerProperties : MonoBehaviour
 {
 
     [Header("Health Status")]
-    public float currentHealth = 100;
-    public float maxHealth = 100;
+    public float currentHealth = 100f;
+    public float maxHealth = 100f;
 
     [Header("Speed values")]
-    public float walkingSpeed;
-    public float runningSpeed;
-    public float sneakMultiplier;
-    public float jumpPower;
+    public float walkingSpeed = 3f;
+    public float runningSpeed = 6f;
+    public float sneakMultiplier = 0.7f;
+    public float jumpPower = 450f;
 
     public float GetCurHealth()
     {
