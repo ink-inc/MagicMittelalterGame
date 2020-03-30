d479a8060dd60f1b098e9f8fd3d9ff8cb303fb2d
kosch104 <Konsicrafter@web.de>
2020-03-30 14:53:27 +0200

    ADD: Basic Inventory manager

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public PlayerProperties playerProperties;
+    private float weight;
+    private int slotsFilled;
+    private List<InventoryItem> inventory;
+
+    public InventoryItem[] inspector_inventory;
+
+    private void Start()
+    {
+        inventory = new List<InventoryItem>();
+    }
+
+    public bool Pickup(InventoryItem item)
+    {
+        if (CanPickup(item.weigth))
+        {
+            inventory.Add(item);
+            return true;
+        }
+        return false;
+    }
+
+    public bool CanPickup(float itemWeight)
+    {
+        //TODO: This is ugly... but it should work
+        return (playerProperties.GetWeightCapacity() < 0 || weight + itemWeight <= playerProperties.GetWeightCapacity()) && (playerProperties.GetSlotCapacity() < 0 || slotsFilled <= playerProperties.GetSlotCapacity());
+    }
+
+    private void UpdateInspectorInventory()
+    {
+        inventory.CopyTo(inspector_inventory);
+    }
+}
