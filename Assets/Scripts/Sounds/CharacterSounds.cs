12ba57cd9f8af58fbf01c0d4044631d29ab952da
Segelzwerg <marcel.haas@hhu.de>
2020-03-30 12:24:13 +0200

    ADD: character sound manager

---
+++ Assets/Scripts/Sounds/CharacterSounds.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Sounds
+{
+    /// <summary>
+    /// This collects all triggers to play sounds for one character.
+    /// </summary>
+    public class CharacterSounds : MonoBehaviour
+    {
+        private const string FoleyPath = "Assets/Sounds/Foleys/";
+
+        [Tooltip("This sound is played when the character receives damage.")]
+        public AudioClip damage;
+        
+        private AudioSource _voiceSources;
+
+        private void Start()
+        {
+            if (damage == null) {}
+
+            {
+                damage = AssetDatabase.LoadAssetAtPath<AudioClip>($"{FoleyPath}404109__deathscyp__damage-1.wav");
+            }
+            _voiceSources = gameObject.AddComponent<AudioSource>();
+        }
+
+        public void Damage()
+        {
+            _voiceSources.clip = damage;
+            _voiceSources.Play();
+        }
+    }
+}
