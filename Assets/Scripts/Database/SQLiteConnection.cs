42808f9f8317fe69b708bc995e2d89f4f3aa26cb
Thabb <49516328+Thabb@users.noreply.github.com>
2020-03-30 16:48:44 +0200

    ADD: Most basic SQLite Database with connection

---
+++ Assets/Scripts/Database/SQLiteConnection.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class SQLiteConnection : MonoBehaviour
+{
+    void Start()
+    {
+        // Create database
+        string connection = "URI=file:" + Application.persistentDataPath + "/My_Database";
+
+        // Open connection
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+
+        // Create table
+        IDbCommand dbcmd;
+        dbcmd = dbcon.CreateCommand();
+        string q_createTable = "CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, val INTEGER )";
+
+        dbcmd.CommandText = q_createTable;
+        dbcmd.ExecuteReader();
+
+        // Inser values in table
+        IDbCommand cmnd = dbcon.CreateCommand();
+        cmnd.CommandText = "INSERT INTO my_table (id, val) VALUES (1, 5)";
+        cmnd.ExecuteNonQuery();
+
+        // Read and print all values in table
+        IDbCommand cmnd_read = dbcon.CreateCommand();
+        IDataReader reader; 
+        string query = "SELECT * FROM my_table";
+        cmnd_read.CommandText = query;
+        reader = cmnd_read.ExecuteReader(); 
+        
+        while (reader.Read())
+        {
+            Debug.Log("id: " + reader[0].ToString());
+            Debug.Log("val: " + reader[1].ToString());
+        }
+
+        //Close connection
+        dbcon.Close();
+    }
+
+    void Update()
+    {
+        
+    }
+}
