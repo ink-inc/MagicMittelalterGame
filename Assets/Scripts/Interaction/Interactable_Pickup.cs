42a54fda8228ad913727d25e7e047e2489914a81
kosch104 <Konsicrafter@web.de>
2020-03-30 15:33:14 +0200

    ADD: Interactable_Pickup

---
+++ Assets/Scripts/Interaction/Interactable_Pickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_Pickup : Interactable
+{
+    public InventoryItem itemToPickup;
+    public Inventory inventory;
+
+    public override void interact()
+    {
+        bool result = inventory.Pickup(itemToPickup);
+        if (result)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
