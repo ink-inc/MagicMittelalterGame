9e7377b9c43e8e8dfba91828a6de178af2ec5266
kosch104 <Konsicrafter@web.de>
2020-03-30 15:34:43 +0200

    FIX: Variable rename references

--- Assets/Scripts/Interaction/Interactable_Healthbar.cs
+++ Assets/Scripts/Interaction/Interactable_Healthbar.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Interactable_Healthbar : Interactable
 {
-
     public PlayerHealthbar playerHealthbar;
     public PlayerProperties playerProp;
     public int value;
+
     public override void interact()
     {
-        playerProp.SetHealth(Mathf.Clamp(playerProp.GetCurHealth() + value, 0, playerProp.GetMaxHealth())); //changes players current health
-        playerHealthbar.SetHealth(playerProp.GetCurHealth());       //adjusts player healthbar
+        playerProp.SetHealth(Mathf.Clamp(playerProp.GetHealth() + value, 0, playerProp.GetMaxHealth())); //changes players current health
+        playerHealthbar.SetHealth(playerProp.GetHealth());       //adjusts player healthbar
     }
 }
--- Assets/Scripts/Interaction/Interactable_PlayerMaxLife.cs
+++ Assets/Scripts/Interaction/Interactable_PlayerMaxLife.cs
@@ -7,13 +7,14 @@ public class Interactable_PlayerMaxLife : Interactable
     public PlayerHealthbar playerHealthbar;
     public PlayerProperties playerProp;
     public int value;
+
     public override void interact()
     {
         if (playerProp.GetMaxHealth() + value > 0)  //players max health should always be below zero
         {
             playerProp.SetMaxHealth(playerProp.GetMaxHealth() + value);     //increases/decreases player max life, depending on value
-            playerHealthbar.SetHealth(playerProp.GetCurHealth());           //adjusts player healthbar 
-            playerProp.SetHealth(Mathf.Clamp(playerProp.GetCurHealth(), 0, playerProp.GetMaxHealth())); //reloads player current life if max life has been decreased below current life
+            playerHealthbar.SetHealth(playerProp.GetHealth());           //adjusts player healthbar
+            playerProp.SetHealth(Mathf.Clamp(playerProp.GetHealth(), 0, playerProp.GetMaxHealth())); //reloads player current life if max life has been decreased below current life
         }
     }
 }
