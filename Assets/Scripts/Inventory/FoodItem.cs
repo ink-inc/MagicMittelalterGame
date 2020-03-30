fad8e792ed8a0de0e6fd585c5d85504105c03248
kosch104 <Konsicrafter@web.de>
2020-03-30 14:36:21 +0200

    ADD: InventoryItem Base class and FoodItem Subclass

---
+++ Assets/Scripts/Inventory/FoodItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodItem : InventoryItem
+{
+    public float healthValue;
+
+    public override void Start()
+    {
+        base.Start();
+        contextActions.Add(action_eat);
+    }
+
+    public void action_eat()
+    {
+        //TODO: Add code for eating
+    }
+}
---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour
+{
+    public delegate void ContextAction();
+
+    public float weigth;
+    public List<ContextAction> contextActions; //Holds all the action for clicking at an item -> Eat, Equip, Drop, etc.
+
+    public virtual void Start()
+    {
+        contextActions = new List<ContextAction>();
+        contextActions.Add(action_drop);
+    }
+
+    public void action_drop()
+    {
+        //TODO: Add Code for dropping item
+    }
+}
