3d29ea549a4c3c94c17e7872d5bc17b4dd052448
Segelzwerg <marcel.haas@hhu.de>
2020-03-30 19:13:25 +0200

    ADD: doc strings

--- Assets/Scripts/Sounds/DoubleAudioSource.cs
+++ Assets/Scripts/Sounds/DoubleAudioSource.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 namespace Sounds
 {
+    /// <summary>
+    /// Uses two audio sources to allow fading between clip.
+    /// To the outside it looks like a Audio Source.
+    /// </summary>
     public class DoubleAudioSource : MonoBehaviour
     {
         private const float FadeDuration = 5f;
@@ -19,9 +23,19 @@ namespace Sounds
             _audioSources = new List<AudioSource>(){firstSource, secondSource};
         }
 
+        /// <summary>
+        /// Boolean if this class is playing some clip.
+        /// </summary>
         public bool IsPlaying => Current().isPlaying;
+        /// <summary>
+        /// The clip current launched in the audio source.
+        /// </summary>
         public AudioClip Clip => Current().clip;
 
+        /// <summary>
+        /// Cross fades to a new clip.
+        /// </summary>
+        /// <param name="clip">The new clip to be played.</param>
         public void CrossFadeToNewClip(AudioClip clip)
         {
             AudioSource fadeFrom = Next();
@@ -33,25 +47,38 @@ namespace Sounds
             StartCoroutine(FadeAudioSource.StartFade(fadeTo, FadeDuration, 1f));
         }
 
+        /// <summary>
+        /// Pauses this audio source.
+        /// </summary>
         public void Pause()
         {
             _audioSources.ForEach(source => source.Pause());
         }
 
+        /// <summary>
+        /// UnPauses this audio source.
+        /// </summary>
         public void UnPause()
         {
             _audioSources.ForEach(source => source.UnPause());
         }
 
+        /// <summary>
+        /// Stops this audio source.
+        /// </summary>
         public void Stop()
         {
             Current().Stop();
         }
 
+  
+        /// <returns>The audio source object which is the currently active one.</returns>
         private AudioSource Current()
         {
             return _audioSources[_index];
         }
+        
+        /// <returns>Returns the next audio source object.</returns>
         private AudioSource Next()
         {
             _index++;
