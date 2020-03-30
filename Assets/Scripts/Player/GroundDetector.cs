18bececa00d7fcae12ae220582eeb5b2c6df0c8c
Segelzwerg <marcel.haas@hhu.de>
2020-03-30 13:17:42 +0200

    ADD: flag for type of ground

--- Assets/Scripts/Player/GroundDetector.cs
+++ Assets/Scripts/Player/GroundDetector.cs
@@ -1,19 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundDetector : MonoBehaviour
 {
+    private const string GroundTypePrefix = "GroundType_";
+    private String _groundType;
     public List<GameObject> currentCollisions = new List<GameObject>();
+
+    public string GroundType => _groundType;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.name != "Underwater PostFX") { 
             currentCollisions.Add(other.gameObject);
         }
+
+        if (other.tag.Contains(GroundTypePrefix))
+        {
+            _groundType = other.tag.Replace(GroundTypePrefix, "");
+        }
     }
 
     public void OnTriggerExit(Collider other)
     {
         currentCollisions.Remove(other.gameObject);
+        
+        if (other.tag.Contains(GroundTypePrefix))
+        {
+            _groundType = null;
+        }
     }
 }
