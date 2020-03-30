c571bf2f5c681b2752e2b8fcdee8781ece2d619c
Segelzwerg <marcel.haas@hhu.de>
2020-03-30 17:42:25 +0200

    ADD: fader for audio sources

---
+++ Assets/Scripts/Sounds/Manager/FadeAudioSource.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+/*
+ * This copied from https://gamedevbeginner.com/how-to-fade-audio-in-unity-i-tested-every-method-this-ones-the-best/#first_method
+ * It was posted on the 29th August 2019 by and retrieved by Marcel Haas (Segelzwerg) on 30th March 2020.
+ */
+namespace Sounds.Manager
+{
+    public static class FadeAudioSource {
+
+        public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+        {
+            float currentTime = 0;
+            float start = audioSource.volume;
+
+            while (currentTime < duration)
+            {
+                currentTime += Time.deltaTime;
+                audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+                yield return null;
+            }
+            yield break;
+        }
+    }
+}
