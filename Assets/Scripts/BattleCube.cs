ab0f0f134581ba4b18c6b98883e809fe98f3b8cd
Segelzwerg <marcel.haas@hhu.de>
2020-04-01 22:03:10 +0200

    ADD: battle area to test scene

---
+++ Assets/Scripts/BattleCube.cs
@@ -0,0 +1,22 @@
+﻿using Sounds.Manager;
+using UnityEngine;
+
+public class BattleCube : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Some one enter battle cube.");
+        if (other.name == "Player")
+        {
+            GameObject.Find("Player").GetComponent<MusicManager>().Fight();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "GroundDetector")
+        {
+            GameObject.Find("Player").GetComponent<MusicManager>().Background("Default");
+        }
+    }
+}
+    
