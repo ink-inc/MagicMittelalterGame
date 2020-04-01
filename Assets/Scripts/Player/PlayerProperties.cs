0c3de4250f223c34f793becfaba20644924cd3e1
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2020-04-01 19:14:59 +0200

    ADD: Weight Penalty

--- Assets/Scripts/Player/PlayerProperties.cs
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -11,8 +11,10 @@ public class PlayerProperties : MonoBehaviour
 
     [Header("Speed values")]
     public float walkingSpeed = 3f;
-
     public float runningSpeed = 6f;
+    private float defaultWalkingSpeed = 3f;
+    private float defaultRunningSpeed = 6f;
+
     public float sneakMultiplier = 0.7f;
     public float jumpPower = 450f;
 
@@ -20,7 +22,8 @@ public class PlayerProperties : MonoBehaviour
     public float weight;
     [Tooltip("Maximum weight capacity of player in kg. Set to negative value for unlimited.")]
     public float weightCapacity = 50;
-
+    [Tooltip("Weight Percentage of maximum capacity at which the player will receive movement penalties (Default 75%). Set to negative vlaue for none")]
+    public float softCap = 75;
     [Tooltip("Maximum slot capacity of player. Set to negative value for unlimited.")]
     public int slotCapacity = -1;
 
@@ -62,5 +65,26 @@ public class PlayerProperties : MonoBehaviour
     public void SetWeight(float value)
     {
         weight = value;
+    }
+
+    public void CalculateSpeed()
+    {
+        float weightCapacityPercentage = weightCapacity / 100;
+        float percentage = weight/weightCapacityPercentage;
+        if (softCap > 0)
+        {
+            if (percentage < softCap)
+            {
+                runningSpeed = defaultRunningSpeed;
+                walkingSpeed = defaultWalkingSpeed;
+            }
+            else
+            {
+                //runningSpeed = defaultRunningSpeed - (defaultRunningSpeed * ((percentage-75)/25));        //linear regression
+                //walkingSpeed = defaultWalkingSpeed - (defaultWalkingSpeed * ((percentage - 75) / 25));
+                runningSpeed = defaultRunningSpeed - (defaultRunningSpeed * Mathf.Pow((((percentage - softCap) / (100 - softCap))), 2f));   //quadratic regression
+                walkingSpeed = defaultWalkingSpeed - (defaultWalkingSpeed * Mathf.Pow((((percentage - softCap) / (100 - softCap))), 2));
+            }
+        }
     }
 }
