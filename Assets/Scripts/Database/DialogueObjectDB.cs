6ad6941249b1905be5d1525636aedffec8462c05
Thabb <49516328+Thabb@users.noreply.github.com>
2020-04-01 23:06:11 +0200

    ADD: SQLite Helper

---
+++ Assets/Scripts/Database/DialogueObjectDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class DialogueObjectDB : SqliteHelper
+{
+    private const string Tag = "Riz: DialogueObjectDB:\t";
+
+    private const string TABLE_NAME = "DialogueObjects";
+    private const string KEY_ID = "id";
+    private const string KEY_TYPE = "type";
+    private const string KEY_DIALOGUELINEIDS = "dialogueLineIds";
+    private string[] COLUMNS = new string[] { KEY_ID, KEY_TYPE, KEY_DIALOGUELINEIDS };
+
+    public DialogueObjectDB() : base()
+    {
+        IDbCommand dbcmd = getDbCommand();
+        dbcmd.CommandText = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( " +
+            KEY_ID + " BIGINT PRIMARY KEY, " +
+            KEY_TYPE + " TEXT, " +
+            KEY_DIALOGUELINEIDS + "  )";
+    }
+}
---
+++ Assets/Scripts/Database/SQLiteHelper.cs
@@ -0,0 +1,105 @@
+﻿using Mono.Data.Sqlite;
+using UnityEngine;
+using System.Data;
+
+public class SqliteHelper
+{
+    private const string Tag = "Riz: SqliteHelper:\t";
+
+    private const string database_name = "my_db";
+
+    public string db_connection_string;
+    public IDbConnection db_connection;
+
+    public SqliteHelper()
+    {
+        db_connection_string = "URI=file:" + Application.persistentDataPath + "/" + database_name;
+        Debug.Log("db_connection_string" + db_connection_string);
+        db_connection = new SqliteConnection(db_connection_string);
+        db_connection.Open();
+    }
+
+    ~SqliteHelper()
+    {
+        db_connection.Close();
+    }
+
+    // virtual functions
+    public virtual IDataReader getDataById(int id)
+    {
+        Debug.Log(Tag + "This function is not implemnted");
+        throw null;
+    }
+
+    public virtual IDataReader getDataByString(string str)
+    {
+        Debug.Log(Tag + "This function is not implemnted");
+        throw null;
+    }
+
+    public virtual void deleteDataById(int id)
+    {
+        Debug.Log(Tag + "This function is not implemented");
+        throw null;
+    }
+
+    public virtual void deleteDataByString(string id)
+    {
+        Debug.Log(Tag + "This function is not implemented");
+        throw null;
+    }
+
+    public virtual IDataReader getAllData()
+    {
+        Debug.Log(Tag + "This function is not implemented");
+        throw null;
+    }
+
+    public virtual void deleteAllData()
+    {
+        Debug.Log(Tag + "This function is not implemnted");
+        throw null;
+    }
+
+    public virtual IDataReader getNumOfRows()
+    {
+        Debug.Log(Tag + "This function is not implemnted");
+        throw null;
+    }
+
+    //helper functions
+    public IDbCommand getDbCommand()
+    {
+        return db_connection.CreateCommand();
+    }
+
+    public IDataReader getAllData(string table_name)
+    {
+        IDbCommand dbcmd = db_connection.CreateCommand();
+        dbcmd.CommandText =
+            "SELECT * FROM " + table_name;
+        IDataReader reader = dbcmd.ExecuteReader();
+        return reader;
+    }
+
+    public void deleteAllData(string table_name)
+    {
+        IDbCommand dbcmd = db_connection.CreateCommand();
+        dbcmd.CommandText = "DROP TABLE IF EXISTS " + table_name;
+        dbcmd.ExecuteNonQuery();
+    }
+
+    public IDataReader getNumOfRows(string table_name)
+    {
+        IDbCommand dbcmd = db_connection.CreateCommand();
+        dbcmd.CommandText =
+            "SELECT COALESCE(MAX(id)+1, 0) FROM " + table_name;
+        IDataReader reader = dbcmd.ExecuteReader();
+        return reader;
+    }
+
+    public void close()
+    {
+        db_connection.Close();
+    }
+}
