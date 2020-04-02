d265bc55f1252cb02e57a59d10168a72fc1a4f31
Thabb <49516328+Thabb@users.noreply.github.com>
2020-04-02 21:42:55 +0200

    UPDATE: DialogueRepository to work with the Database
    Instead of just returning DummyData

--- Assets/Scripts/Dialogue/DialogueRepository.cs
+++ Assets/Scripts/Dialogue/DialogueRepository.cs
@@ -1,4 +1,6 @@
-﻿public class DialogueRepository
+﻿using System.Linq;
+using UnityEngine;
+public class DialogueRepository
 {
     public void ReadData() { // just a method for testing
                 
@@ -6,92 +8,33 @@
 
     public DialogueObject ReadDialogueObjectById(int id) {
         // Database call
+        DialogueObjectDB dialogueObjectDB = new DialogueObjectDB();
 
+        System.Data.IDataReader reader = dialogueObjectDB.getDataById(id);
 
-        // Dummy data
-        if (id == 1)
-        {
-            int[] array = {1};
-            return new DialogueObject(id, "Line", array);
-        } 
-        else if (id == 2)
-        {
-            int[] array = { 2 };
-            return new DialogueObject(id, "Line", array);
-        } 
-        else if (id == 3)
-        {
-            int[] array = { 3, 4, 5 };
-            return new DialogueObject(id, "Decision", array);
-        } 
-        else if (id == 4)
-        {
-            int[] array = { 6 };
-            return new DialogueObject(id, "Line", array);
-        } 
-        else if (id == 5)
-        {
-            int[] array = { 7 };
-            return new DialogueObject(id, "Line", array);
-        } 
-        else if (id == 6)
-        {
-            int[] array = { 8 };
-            return new DialogueObject(id, "Line", array);
-        } 
-        else if (id == 7)
-        {
-            int[] array = { };
-            return new DialogueObject(id, "End", array);
-        }
-        else
-        {
-            return new DialogueObject();
-        }
+        DialogueObject dialogueObject = new DialogueObject(
+            int.Parse(string.Format("{0}", reader[0])),
+            string.Format("{0}", reader[1]),
+            string.Format("{0}", reader[2]).Split(',').Select(int.Parse).ToArray());
 
+        dialogueObjectDB.close();
 
+        return dialogueObject;
     }
 
     public DialogueLine ReadDialogueLineById(int id)
     {
-        // Database call
+        DialogueLineDB dialogueLineDB = new DialogueLineDB();
 
-        // Dummy Data
-        if (id == 1)
-        {
-            return new DialogueLine(1, "Line 1", 2);
-        } 
-        else if (id == 2)
-        {
-            return new DialogueLine(2, "Line 2", 3);
-        }
-        else if (id == 3)
-        {
-            return new DialogueLine(3, "Decision 1", 4);
-        }
-        else if (id == 4)
-        {
-            return new DialogueLine(4, "Decision 2", 5);
-        }
-        else if (id == 5)
-        {
-            return new DialogueLine(5, "Decision 3", 6);
-        }
-        else if (id == 6)
-        {
-            return new DialogueLine(6, "Line 6", 7);
-        }
-        else if (id == 7)
-        {
-            return new DialogueLine(7, "Line 7", 7);
-        }
-        else if (id == 8)
-        {
-            return new DialogueLine(8, "Line 8", 7);
-        }
-        else
-        {
-            return new DialogueLine();
-        }    
+        System.Data.IDataReader reader = dialogueLineDB.getDataById(id);
+
+        DialogueLine dialogueLine = new DialogueLine(
+            int.Parse(string.Format("{0}", reader[0])),
+            string.Format("{0}", reader[1]),
+            int.Parse(string.Format("{0}", reader[2])));
+
+        dialogueLineDB.close();
+
+        return dialogueLine;
     }
 }
