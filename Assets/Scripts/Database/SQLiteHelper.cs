479ade650e2fd3861d24b9236b8efc3f06b586c9
Thabb <49516328+Thabb@users.noreply.github.com>
2020-04-06 16:09:30 +0200

    UPDATE: Database location and handling

--- Assets/Scripts/Database/SQLiteHelper.cs
+++ Assets/Scripts/Database/SQLiteHelper.cs
@@ -13,7 +13,7 @@ public class SqliteHelper
 
     public SqliteHelper()
     {
-        db_connection_string = "URI=file:" + Application.dataPath + "/DB/" + database_name;
+        db_connection_string = "URI=file:" + Application.streamingAssetsPath + "/" + database_name;
         Logger.log("db_connection_string" + db_connection_string);
         db_connection = new SqliteConnection(db_connection_string);
         db_connection.Open();
