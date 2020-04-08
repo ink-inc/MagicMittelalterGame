df155c9a0bae689e1b51deaaeff2d6708be3333f
Segelzwerg <marcel.haas@hhu.de>
2020-04-08 14:31:39 +0200

    REFACTOR: properties can be fields

--- Assets/Scripts/Sounds/AudioSettings.cs
+++ Assets/Scripts/Sounds/AudioSettings.cs
@@ -7,20 +7,21 @@ namespace Sounds
     {
         [Tooltip("Audio master of the scene.")]
         public AudioMixer master;
-        
-        private AudioMixerGroup Music { get; set; }
-        private AudioMixerGroup Dialogue { get; set; }
-        private AudioMixerGroup Character { get; set; }
-        private AudioMixerGroup Ambient { get; set; }
-        private AudioMixerGroup Effects { get; set; }
+
+        private AudioMixerGroup _music;
+        private AudioMixerGroup _dialogue;
+        private AudioMixerGroup _character;
+        private AudioMixerGroup _ambient;
+        private AudioMixerGroup _effects;
+
 
         private void Start()
         {
-            Music = master.FindMatchingGroups("Music")[0];
-            Dialogue = master.FindMatchingGroups("Dialogue")[0];
-            Character = master.FindMatchingGroups("Character")[0];
-            Ambient = master.FindMatchingGroups("Ambient")[0];
-            Effects = master.FindMatchingGroups("Effects")[0];
+            _music = master.FindMatchingGroups("Music")[0];
+            _dialogue = master.FindMatchingGroups("Dialogue")[0];
+            _character = master.FindMatchingGroups("Character")[0];
+            _ambient = master.FindMatchingGroups("Ambient")[0];
+            _effects = master.FindMatchingGroups("Effects")[0];
         }
 
         public void ChangeMaster(float value)
@@ -30,24 +31,24 @@ namespace Sounds
         
         public void ChangeMusic(float value)
         {
-            Music.audioMixer.SetFloat("MusicVolume", value);
+            _music.audioMixer.SetFloat("MusicVolume", value);
         }
         
         public void ChangeDialogue(float value)
         {
-            Dialogue.audioMixer.SetFloat("DialogueVolume", value);
+            _dialogue.audioMixer.SetFloat("DialogueVolume", value);
         }
         public void ChangeCharacter(float value)
         {
-            Character.audioMixer.SetFloat("CharacterVolume", value);
+            _character.audioMixer.SetFloat("CharacterVolume", value);
         }
         public void ChangeAmbient(float value)
         {
-            Ambient.audioMixer.SetFloat("AmbientVolume", value);
+            _ambient.audioMixer.SetFloat("AmbientVolume", value);
         }
         public void ChangeEffects(float value)
         {
-            Effects.audioMixer.SetFloat("EffectsVolume", value);
+            _effects.audioMixer.SetFloat("EffectsVolume", value);
         }
     }
 }
