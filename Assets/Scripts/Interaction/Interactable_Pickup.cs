380e187e1a594f936eab61f5019e0753ada47228
kosch104 <Konsicrafter@web.de>
2020-04-08 12:15:32 +0200

    UPDATE: Pickup script to override DisplaySubText

--- Assets/Scripts/Interaction/Interactable_Pickup.cs
+++ Assets/Scripts/Interaction/Interactable_Pickup.cs
@@ -7,6 +7,11 @@ public class Interactable_Pickup : Interactable
     public InventoryItem itemToPickup;
     public Inventory inventory;
 
+    private void Awake()
+    {
+        displaySubtext = "[E] to pick up"; //Override subtext for all pickupables
+    }
+
     public override void interact()
     {
         itemToPickup.gameObject.SetActive(!inventory.Pickup(itemToPickup));
