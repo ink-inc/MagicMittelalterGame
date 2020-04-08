35ac26bcc8028aa6a77d851aaad4c303f3dbf98d
Segelzwerg <marcel.haas@hhu.de>
2020-04-08 14:42:06 +0200

    ADD: doc string

--- Assets/Scripts/Sounds/AudioSettings.cs
+++ Assets/Scripts/Sounds/AudioSettings.cs
@@ -3,6 +3,9 @@ using UnityEngine.Audio;
 
 namespace Sounds
 {
+    /// <summary>
+    /// Handles all audio settings.
+    /// </summary>
     public class AudioSettings: MonoBehaviour
     {
         [Tooltip("Audio master of the scene.")]
@@ -24,28 +27,50 @@ namespace Sounds
             _effects = master.FindMatchingGroups("Effects")[0];
         }
 
+        /// <summary>
+        /// Changes the master volume.
+        /// </summary>
+        /// <param name="value">The volume to change too. Must be in [-80, 20]</param>
         public void ChangeMaster(float value)
         {
             master.SetFloat("MasterVolume", value);
         }
-        
+        /// <summary>
+        /// Changes the music volume.
+        /// </summary>
+        /// <param name="value">The volume to change too. Must be in [-80, 20]</param>
         public void ChangeMusic(float value)
         {
             if (_music != null) _music.audioMixer.SetFloat("MusicVolume", value);
         }
-        
+        /// <summary>
+        /// Changes the dialogue volume.
+        /// </summary>
+        /// <param name="value">The volume to change too. Must be in [-80, 20]</param>
         public void ChangeDialogue(float value)
         {
             if (_dialogue != null) _dialogue.audioMixer.SetFloat("DialogueVolume", value);
         }
+        /// <summary>
+        /// Changes the character volume.
+        /// </summary>
+        /// <param name="value">The volume to change too. Must be in [-80, 20]</param>
         public void ChangeCharacter(float value)
         {
             if (_character != null) _character.audioMixer.SetFloat("CharacterVolume", value);
         }
+        /// <summary>
+        /// Changes the ambient volume.
+        /// </summary>
+        /// <param name="value">The volume to change too. Must be in [-80, 20]</param>
         public void ChangeAmbient(float value)
         {
             if (_ambient != null) _ambient.audioMixer.SetFloat("AmbientVolume", value);
         }
+        /// <summary>
+        /// Changes the effects volume.
+        /// </summary>
+        /// <param name="value">The volume to change too. Must be in [-80, 20]</param>
         public void ChangeEffects(float value)
         {
             if (_effects != null) _effects.audioMixer.SetFloat("EffectsVolume", value);
