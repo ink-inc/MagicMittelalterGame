2fe8ee183760ddbd739e12546494826b508d45f5
Segelzwerg <marcel.haas@hhu.de>
2020-04-15 18:07:13 +0200

    ADD: trigger for enter and leaving sound areas

--- Assets/Scripts/Areas/SoundArea.cs
+++ Assets/Scripts/Areas/SoundArea.cs
@@ -1,5 +1,6 @@
 using System;
 using Sounds;
+using Sounds.Manager;
 using UnityEngine;
 
 namespace Areas
@@ -9,5 +10,17 @@ namespace Areas
         [Tooltip("Name of the playlist to be played.")][Obsolete] public string playlist;
         [Tooltip("Load a playlist into it, which will be played upon entering.")]
         public PlaylistScriptable playlistScriptable;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.name != "Player") return;
+            GameObject.Find("Player").GetComponent<MusicManager>().PlayPlaylist(playlistScriptable);
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.name != "Player") return;
+            GameObject.Find("Player").GetComponent<MusicManager>().PlayPlaylist();
+        }
     }
 }   
