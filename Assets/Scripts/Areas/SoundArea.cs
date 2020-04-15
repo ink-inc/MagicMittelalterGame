41db67edda641798ad84d455ef926a00c12edf0a
Segelzwerg <marcel.haas@hhu.de>
2020-04-15 17:38:42 +0200

    UPDATE: add tooltip and mark string deprecated

--- Assets/Scripts/Areas/SoundArea.cs
+++ Assets/Scripts/Areas/SoundArea.cs
@@ -1,9 +1,13 @@
+using System;
+using Sounds;
 using UnityEngine;
 
 namespace Areas
 {
     public abstract class SoundArea : MonoBehaviour
     {
-        [Tooltip("Name of the playlist to be played.")] public string playlist;
+        [Tooltip("Name of the playlist to be played.")][Obsolete] public string playlist;
+        [Tooltip("Load a playlist into it, which will be played upon entering.")]
+        public PlaylistScriptable playlistScriptable;
     }
 }   
