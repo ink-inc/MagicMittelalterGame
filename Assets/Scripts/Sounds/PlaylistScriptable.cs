a47a57ac10898e6c2931bae0b57fa91bb0bbb4c3
Segelzwerg <marcel.haas@hhu.de>
2020-04-15 17:53:57 +0200

    ADD: name attribute

--- Assets/Scripts/Sounds/PlaylistScriptable.cs
+++ Assets/Scripts/Sounds/PlaylistScriptable.cs
@@ -6,6 +6,7 @@ namespace Sounds
     [CreateAssetMenu(fileName = "Playlist", menuName = "ScriptableObjects/PlaylistScriptable", order = 1)]
     public class PlaylistScriptable : ScriptableObject
     {
+        public new string name;
         public List<AudioClip> playlist;
     }
 }
