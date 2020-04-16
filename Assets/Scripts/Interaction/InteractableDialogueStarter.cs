3eb7356e7fa533f75d484c99baf5612cce2102ca
iTitus <iTitus@users.noreply.github.com>
2020-04-16 16:29:32 +0200

    FIX: character sounds loading

--- Assets/Scripts/Interaction/InteractableDialogueStarter.cs
+++ Assets/Scripts/Interaction/InteractableDialogueStarter.cs
@@ -8,10 +8,19 @@ namespace Interaction
     {
         public int dialogueId;
         public DialogueHandler dialogueHandler;
+        public CharacterSounds characterSounds;
+
+        private void Start()
+        {
+            if (characterSounds == null)
+            {
+                characterSounds = GetComponent<CharacterSounds>();
+                Logger.logWarning("Interactable characterSounds is missing and has been automatically assigned. Please assign it manually.");
+            }
+        }
 
         public override void Interact(Interactor interactor)
         {
-            CharacterSounds characterSounds = GetComponent<CharacterSounds>();
             dialogueHandler.StartDialogue(dialogueId, characterSounds);
         }
     }
