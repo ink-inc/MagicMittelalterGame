8804d86c3d0f3cf71ff667601b03f3ea3e342f69
Segelzwerg <marcel.haas@hhu.de>
2020-04-27 20:21:39 +0200

    ADD: map entry object

--- Assets/Scripts/AI/MapEntry.cs
+++ Assets/Scripts/AI/MapEntry.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace AI
 {
     public class MapEntry
     {
-        
+        public MapEntry(List<float> attributes)
+        {
+            Attributes = attributes;
+        }
+
+        public List<float> Attributes { get; }
+
+        public int Dimension() => Attributes.Count;
     }
 }
