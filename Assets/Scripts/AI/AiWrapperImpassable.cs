fbcfd3e547bb871be3fcadc905f269040eba2e55
Segelzwerg <marcel.haas@hhu.de>
2020-04-28 01:03:55 +0200

    ADD: type to impassable

--- Assets/Scripts/AI/AiWrapperImpassable.cs
+++ Assets/Scripts/AI/AiWrapperImpassable.cs
@@ -1,11 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AI
 {
     public class AiWrapperImpassable : AiWrapper
     {
-        public override MapEntry MapEntry => new MapEntry(null);
-        
+        private readonly SortedDictionary<string, float> _attributes;
+
+        public AiWrapperImpassable()
+        {
+            _attributes = new SortedDictionary<string, float> {{"type", 1f}};
+        }
+
+        public override MapEntry MapEntry => new MapEntry(_attributes);
+
 
         public override Vector3 Position => transform.position;
     }
