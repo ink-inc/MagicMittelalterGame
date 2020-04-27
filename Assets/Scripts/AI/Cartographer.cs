83e0d5ac630dc885e14e9535e1346ba949504d6b
Segelzwerg <marcel.haas@hhu.de>
2020-04-27 12:04:28 +0200

    ADD: matrix property

--- Assets/Scripts/AI/Cartographer.cs
+++ Assets/Scripts/AI/Cartographer.cs
@@ -19,11 +19,30 @@ namespace AI
         {
             _map = new List<float>[width, height];
         }
+        
+        private List<List<float>> Matrix
+        {
+            get
+            {
+                FillStaticMap();
+                List<List<float>> matrix = new List<List<float>>();
+
+                for (int y = 0; y < _map.GetLength(0); y++)
+                {
+                    for (int x = 0; x < _map.GetLength(1); x++)
+                    {
+                        matrix.Add(_map[x, y]);
+                    }
+                }
+
+                return matrix;
+            }
+        }
 
         /// <summary>
         /// Creates a map of all static game objects in the scene.
         /// </summary>
-        public void FillStaticMap()
+        private void FillStaticMap()
         {
             _gameObjects.AddRange(GameObject.FindGameObjectsWithTag("Interactable").ToList());
             foreach (GameObject gameObject in _gameObjects)
@@ -35,6 +54,8 @@ namespace AI
                 _map[x, y] = InformationOf(gameObject);
             }
         }
+
+        
 
         /// <summary>
         /// Retrieves the information of an object as a list of floats.
