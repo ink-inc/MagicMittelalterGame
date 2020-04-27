749623482758be7976f5ae7082116b15d12b5213
Segelzwerg <marcel.haas@hhu.de>
2020-04-28 01:03:37 +0200

    ADD: map object

---
+++ Assets/Scripts/AI/Map.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+
+namespace AI
+{
+    public class Map
+    {
+        private readonly MapEntry[,] _map;
+        private readonly int _halfX;
+        private readonly int _halfY;
+
+        public Map(int width, int height)
+        {
+            _halfX = width;
+            _halfY = height;
+            int widthNorm = width * 2;
+            int heightNorm = height * 2;
+            _map = new MapEntry[widthNorm, heightNorm];
+            for (int i = 0; i < heightNorm; i++)
+            {
+                for (int j = 0; j < widthNorm; j++)
+                {
+                    _map[i, j] =  new MapEntry(null);
+                }
+            }
+        }
+
+        public void SetEntry(int x, int y, MapEntry mapEntry)
+        {
+            _map[x + _halfX, y + _halfY] = mapEntry;
+        }
+
+        public List<MapEntry> ToList()
+        {
+            List<MapEntry> mapEntries = new List<MapEntry>();
+            for (int j = 0; j < _map.GetLength(1); j++)
+            {
+                for (int i = 0; i < _map.GetLength(0); i++)
+                {
+                    mapEntries.Add(_map[i,j]);
+                }
+            }
+
+            return mapEntries;
+        }
+    }
+}
