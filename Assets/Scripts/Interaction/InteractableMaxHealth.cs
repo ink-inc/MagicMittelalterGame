e2c88a1a21cb9d0c8648da9f790a3be4e32610da
Thabb <49516328+Thabb@users.noreply.github.com>
2020-04-28 19:18:49 +0200

    UPDATE: The damage taken by an enemy is now displayed in the enemyhealthbar

--- Assets/Scripts/Interaction/InteractableMaxHealth.cs
+++ Assets/Scripts/Interaction/InteractableMaxHealth.cs
@@ -14,7 +14,6 @@ namespace Interaction
             if (playerProp.GetMaxHealth() + value > 0)  //players max health should always be below zero
             {
                 playerProp.SetMaxHealth(playerProp.GetMaxHealth() + value);     //increases/decreases player max life, depending on value
-                playerHealthbar.SetHealth(playerProp.GetHealth());           //adjusts player healthbar
                 playerProp.SetHealth(Mathf.Clamp(playerProp.GetHealth(), 0, playerProp.GetMaxHealth())); //reloads player current life if max life has been decreased below current life
             }
         }
--- Assets/Scripts/Player/PlayerHealthbar.cs
+++ Assets/Scripts/Player/PlayerHealthbar.cs
@@ -7,20 +7,13 @@ public class PlayerHealthbar : MonoBehaviour
 {
     public Image healthbarBack;
     public Image healthbarFront;
-    public PlayerProperties prop;
 
-    public void SetHealth(float currentHealth)  //Adjusts red health bar to current health
+    public void SetHealth(float currentHealth, float maxHealth)  //Adjusts red health bar to current health
     {
-        float maxHealth = prop.GetMaxHealth();
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         float maxHealthPercentage = maxHealth / 100;
         float healthbarPercentageFilled = currentHealth / maxHealthPercentage;
         float absoluteValue = healthbarPercentageFilled * (healthbarBack.rectTransform.sizeDelta.x / 100); //Calculation: Percentage * (width of parent / 100) -> width for child
         healthbarFront.rectTransform.sizeDelta = new Vector2(absoluteValue, 20);
-    }
-
-    public void Refresh()
-    {
-        SetHealth(prop.GetHealth());
     }
 }
