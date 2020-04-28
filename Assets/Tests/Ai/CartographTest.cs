02e4aff3d35a33dc33feb57d32134594e9165d0a
Segelzwerg <marcel.haas@hhu.de>
2020-04-28 14:45:36 +0200

    FIX: renaming scene cause deletion

--- Assets/Tests/Ai/CartographTest.cs
+++ Assets/Tests/Ai/CartographTest.cs
@@ -11,7 +11,7 @@ namespace Tests.Ai
         [UnityTest]
         public IEnumerator MapAiArenaTest()
         {
-            SceneManager.LoadScene("AiArena");
+            SceneManager.LoadScene("TestScene 1");
             yield return null;
             Cartographer cartographer = new Cartographer(5, 5);
             float[,] matrixNnReady = cartographer.MatrixNnReady();
