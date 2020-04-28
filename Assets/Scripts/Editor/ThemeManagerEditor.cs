038398fd687ab1f34795b74ce86ef595640e14d5
kosch104 <Konsicrafter@web.de>
2020-04-28 11:24:39 +0200

    ADD: Apply Theme Functionality to Theme Manager

--- Assets/Scripts/Editor/ThemeManagerEditor.cs
+++ Assets/Scripts/Editor/ThemeManagerEditor.cs
@@ -12,16 +12,22 @@ namespace Theme
         {
             base.OnInspectorGUI();
 
-            /*ThemeManager themeManager = (ThemeManager)target;
+            ThemeManager themeManager = (ThemeManager)target;
             GUILayout.Space(50);
+
+            GUILayout.BeginHorizontal();
             GUILayout.Label("Select a Theme Index to apply");
             themeManager.indexToApply = int.Parse(EditorGUILayout.TextField(themeManager.indexToApply.ToString()));
-
             if (GUILayout.Button("Apply Theme"))
             {
+                if (themeManager.indexToApply < 0 || themeManager.indexToApply >= themeManager.selectableThemes.Length)
+                {
+                    themeManager.indexToApply = 0;
+                    return;
+                }
                 ThemeManager.ApplyTheme(themeManager.indexToApply);
             }
-            this.Repaint();*/
+            GUILayout.EndHorizontal();
         }
     }
 }
