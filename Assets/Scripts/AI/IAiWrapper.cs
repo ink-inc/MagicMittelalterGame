4dab74b13871325074f963243c7ecf1f22552baf
Segelzwerg <marcel.haas@hhu.de>
2020-04-29 20:09:30 +0200

    UPDATE: AiWrapper is no longer abstract

--- Assets/Scripts/AI/IAiWrapper.cs
+++ Assets/Scripts/AI/IAiWrapper.cs
@@ -1,11 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AI
 {
-    public abstract class AiWrapper : MonoBehaviour
+    public class AiWrapper : MonoBehaviour
     {
-        public virtual MapEntry MapEntry { get; }
-        public virtual Vector3 Position { get; }
-        public virtual Vector3 Size { get; }
+        public AiWrapper(List<string> attributeKeys)
+        {
+            MapEntry = SetMapEntry(attributeKeys);
+            Transform localTransform = transform;
+            Position = localTransform.position;
+            Size = localTransform.localScale;
+        }
+
+        private MapEntry SetMapEntry(List<string> attributeKeys)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public MapEntry MapEntry { get; }
+        public Vector3 Position { get; }
+        public Vector3 Size { get; }
     }
 }
