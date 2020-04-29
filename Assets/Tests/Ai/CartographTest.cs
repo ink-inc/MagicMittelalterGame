f689ed237c499244b29fd21de30f15cdc714d3c4
Segelzwerg <marcel.haas@hhu.de>
2020-04-29 12:41:08 +0200

    UPDATE: test for walkable fields as well

--- Assets/Tests/Ai/CartographTest.cs
+++ Assets/Tests/Ai/CartographTest.cs
@@ -21,31 +21,26 @@ namespace Tests.Ai
             Assert.AreEqual(121,matrixNnReady.GetLength(0));
             Assert.AreEqual(1,matrixNnReady.GetLength(1));
             
-            IEnumerable<int> northWall = Enumerable.Range(0, 11);
-            foreach (int i in northWall)
+            IEnumerable<int> northWall = Enumerable.Range(0, 11).ToList();
+            IEnumerable<int> westWall = Enumerable.Range(0, 11).Select(x => x * 11).ToList();
+            IEnumerable<int> eastWall = Enumerable.Range(1, 11).Select(x => x * 11 - 1).ToList();
+            IEnumerable<int> southWall = Enumerable.Range(110, 11).ToList();
+
+            IEnumerable<int> allWalls = northWall.Concat(eastWall).Concat(southWall).Concat(westWall).ToList();
+            
+
+            List<int> allFields = Enumerable.Range(0, matrixNnReady.GetLength(0)).ToList();
+            IEnumerable<int> allWalkable = allFields.Where(x => allWalls.Contains(x) == false);
+            
+            foreach (int i in allWalls)
             {
                 Assert.AreEqual(1f, matrixNnReady[i, 0], message: $"Not 1f at {i}");
 
             }
-
-            IEnumerable<int> westWall = Enumerable.Range(0, 11).Select(x => x * 11);
-            foreach (int i in westWall)
+            
+            foreach (int i in allWalkable)
             {
-                Assert.AreEqual(1f, matrixNnReady[i, 0], message: $"Not 1f at {i}");
-
-            }
-
-            IEnumerable<int> eastWall = Enumerable.Range(1, 11).Select(x => x * 11 - 1);
-            foreach (int i in eastWall)
-            {
-                Assert.AreEqual(1f, matrixNnReady[i, 0], message: $"Not 1f at {i}");
-
-            }
-
-            IEnumerable<int> southWall = Enumerable.Range(110, 11);
-            foreach (int i in southWall)
-            {
-                Assert.AreEqual(1f, matrixNnReady[i, 0], message: $"Not 1f at {i}");
+                Assert.AreEqual(0f, matrixNnReady[i, 0], message: $"Not 0f at {i}");
 
             }
         }
