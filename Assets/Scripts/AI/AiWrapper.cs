0a0a13f8266dc39336863389138ad35d6ab8b16f
Segelzwerg <marcel.haas@hhu.de>
2020-04-29 21:54:51 +0200

    UPDATE: attributes interface

--- Assets/Scripts/AI/AiWrapper.cs
+++ Assets/Scripts/AI/AiWrapper.cs
@@ -5,21 +5,50 @@ namespace AI
 {
     public class AiWrapper : MonoBehaviour
     {
-        public AiWrapper(List<string> attributeKeys)
+        private readonly Rigidbody _rigidbody;
+        public TeamScriptable team;
+
+        public AiWrapper()
         {
-            MapEntry = SetMapEntry(attributeKeys);
             Transform localTransform = transform;
             Position = localTransform.position;
             Size = localTransform.localScale;
+            _rigidbody = GetComponent<Rigidbody>();
         }
 
-        private MapEntry SetMapEntry(List<string> attributeKeys)
+        private MapEntry GenerateAttributeList(int teamId)
         {
-            throw new System.NotImplementedException();
+            Vector3 velocity = _rigidbody.velocity;
+            Dictionary<string, float> attributes = new Dictionary<string, float>
+            {
+                {"team", team.calculateTeamRelation(teamId)},
+                {"health", GetHealth()},
+                {"armor", GetArmor()},
+                {"vecX", velocity.x},
+                {"vecY", velocity.y},
+                {"vecZ", velocity.z}
+            };
+            
+            return new MapEntry(attributes);
         }
 
-        public MapEntry MapEntry { get; }
+        public MapEntry MapEntry(int teamId)
+        {
+            return GenerateAttributeList(teamId);
+        }
         public Vector3 Position { get; }
         public Vector3 Size { get; }
+        
+        private float GetHealth()
+        {
+            Logger.logWarning("Programming Team must implement this.");
+            return 0f;
+        }
+        
+        private float GetArmor()
+        {
+            Logger.logWarning("Programming Team must implement this.");
+            return 0f;
+        }
     }
 }
