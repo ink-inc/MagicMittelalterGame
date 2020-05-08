1df63d316180b67d9879e3122508d31a6f6c9c42
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2020-05-08 23:27:05 +0200

    ADD: Database for Quest and QuestStage

--- Assets/Scripts/Interaction/Interactable_StartQuest.cs
+++ Assets/Scripts/Interaction/Interactable_StartQuest.cs
@@ -10,6 +10,7 @@ namespace Interaction
         public Questlog questlog;
         public QuestRepository questRepository;
         public int questId;
+        
         public override void Interact(Interactor interactor )
         {
             questHandler.TryStartQuest(questId);
