536e62bc97dafed7adc0761ecc3e699a6ca7fb1d
Segelzwerg <marcel.haas@hhu.de>
2020-05-09 21:48:36 +0200

    UPDATE: use team id from properties

--- Assets/Scripts/AI/AiWrapper.cs
+++ Assets/Scripts/AI/AiWrapper.cs
@@ -20,13 +20,13 @@ namespace AI
             _characterProperties = GetComponent<CharacterProperties>();
         }
 
-        private MapEntry GenerateAttributeList(int teamId)
+        private MapEntry GenerateAttributeList()
         {
             Vector3 velocity = _rigidbody != null ? _rigidbody.velocity : Vector3.zero;
             
             Dictionary<string, float> attributes = new Dictionary<string, float>
                 {
-                    {"team", GetTeamRelation(teamId)},
+                    {"team", GetTeamRelation()},
                     {"health", GetHealth()},
                     {"armor", GetArmor()},
                     {"vecX", velocity.x},
@@ -40,14 +40,15 @@ namespace AI
 
         public MapEntry MapEntry(int teamId)
         {
-            return GenerateAttributeList(teamId);
+            return GenerateAttributeList();
         }
         public Vector3 Position { get; private set; }
         public Vector3 Size { get; private set; }
 
-        private float GetTeamRelation(int teamId)
+        private float GetTeamRelation()
         {
-            return teamId;
+            return _characterProperties != null ? _characterProperties.team.Value : 0f;
+
         }
         private float GetHealth()
         {
