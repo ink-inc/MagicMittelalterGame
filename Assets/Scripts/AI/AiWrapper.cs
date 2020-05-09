337ea76817c03c2becdf19a17a2220c60c7697cc
Segelzwerg <marcel.haas@hhu.de>
2020-05-09 12:42:11 +0200

    UPDATE: use player properties

--- Assets/Scripts/AI/AiWrapper.cs
+++ Assets/Scripts/AI/AiWrapper.cs
@@ -1,18 +1,13 @@
 using System.Collections.Generic;
-using Stat;
 using UnityEngine;
-using Util;
 
 namespace AI
 {
-    [RequireComponent(typeof(AttributeHolder))]
+    [RequireComponent(typeof(PlayerProperties))]
     public class AiWrapper : MonoBehaviour
     {
         private Rigidbody _rigidbody;
-        private AttributeHolder _attributeHolder;
-        private AttributeType _health;
-        private AttributeType _armor;
-        private AttributeType _team;
+        private PlayerProperties _playerProperties;
 
         public void Start()
         {
@@ -20,10 +15,7 @@ namespace AI
             Position = localTransform.position;
             Size = localTransform.localScale;
             TryGetComponent(out _rigidbody);
-            _attributeHolder = GetComponent<AttributeHolder>();
-            _health = AttributeType.Create("Health");
-            _armor = AttributeType.Create("Armor");
-            _team = AttributeType.Create("Team");
+            _playerProperties = GetComponent<PlayerProperties>();
         }
 
         private MapEntry GenerateAttributeList(int teamId)
@@ -53,20 +45,16 @@ namespace AI
 
         private float GetTeamRelation(int teamId)
         {
-            _attributeHolder.TryGetAttribute(_team, out Float teamAttribute);
             return teamId;
         }
         private float GetHealth()
         {
-            _attributeHolder.TryGetAttribute(_health, out Float healthAttribute);
-
-            return healthAttribute.Value;
+            return _playerProperties.health.Value;
         }
         
         private float GetArmor()
         {
-            _attributeHolder.TryGetAttribute(_armor, out Float armorAttribute);
-            return armorAttribute.Value;
+            return _playerProperties.armor.Value;
         }
     }
 }
