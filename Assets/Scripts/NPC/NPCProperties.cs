9a7f597ee666abecef1f258e8de4bd6c9e291957
Segelzwerg <marcel.haas@hhu.de>
2020-05-09 13:31:36 +0200

    ADD: attribute types to properties

--- Assets/Scripts/NPC/NPCProperties.cs
+++ Assets/Scripts/NPC/NPCProperties.cs
@@ -1,4 +1,5 @@
-using System;
+using Stat;
+using Util;
 
 namespace NPC
 {
@@ -6,7 +7,19 @@ namespace NPC
     {
         private void Start()
         {
-            throw new NotImplementedException();
+            health = FloatVariable.Create(10f, "Health");
+            maxHealth = StatAttribute.Create(10f, attributeType: "MaxHealth");
+            
+            speed = StatAttribute.Create(3f, "Speed");
+            sneakMultiplier = 0.7f;
+            runMultiplier = 2f;
+
+            jumpPower = 450f;
+            
+            weight = StatAttribute.Create(0, "Weight");
+            maxWeight = StatAttribute.Create(0, "MaxWeight");
+
+            slotCapacity = -1;
         }
     }
 }
