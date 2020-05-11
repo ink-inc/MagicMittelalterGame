f53dca63217eb1b457771a86e83baac1a55a42df
Segelzwerg <marcel.haas@hhu.de>
2020-05-11 12:59:40 +0200

    FIX: default is no scaling

--- Assets/Scripts/AI/BaseAgent.cs
+++ Assets/Scripts/AI/BaseAgent.cs
@@ -15,7 +15,7 @@ namespace AI
     [RequireComponent(typeof(CharacterProperties))]
     public class BaseAgent : Agent
     {
-        private int _scale = 2;
+        private int _scale = 1;
         private Cartographer _cartographer;
 
         public List<string> AttributeKeys
