3b42c6348efa53a2a1614ae0fee10b149e57920f
Segelzwerg <marcel.haas@hhu.de>
2020-05-11 10:41:10 +0200

    ADD: debug channel

---
+++ Assets/Scripts/AI/DebugSideChannel.cs
@@ -0,0 +1,28 @@
+using System;
+using Unity.MLAgents.SideChannels;
+
+namespace AI
+{
+    public class DebugSideChannel : SideChannel
+    {
+        public DebugSideChannel()
+        {
+            ChannelId = new Guid("0612127d-99fa-4b18-a195-7a49e048f2d6");
+        }
+        protected override void OnMessageReceived(IncomingMessage msg)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void SendDebugLine(string line)
+        {
+            using (OutgoingMessage message = new OutgoingMessage())
+            {
+                message.WriteString(line);
+                QueueMessageToSend(message);
+            }
+        }
+    }
+    
+    
+}
