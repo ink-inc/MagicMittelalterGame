e812627fe17c8a092ed3a3f41406ed8d7d7195d4
Segelzwerg <marcel.haas@hhu.de>
2020-05-12 16:10:34 +0200

    FIX: team must be changeable

--- Assets/Scripts/Character/NPC/NpcProperties.cs
+++ Assets/Scripts/Character/NPC/NpcProperties.cs
@@ -8,7 +8,7 @@ namespace Character.NPC
         protected override void Start()
         {
             base.Start();
-            team = StatAttribute.Create(1f, "Team");
+            team = FloatVariable.Create(1f, "Team");
 
             maxHealth = StatAttribute.Create(10f, "MaxHealth");
             health = FloatVariable.Create(FloatConstant.Create(maxHealth.Value), FloatConstant.Create(0), maxHealth,
