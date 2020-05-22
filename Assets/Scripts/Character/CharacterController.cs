bdcecb5ee8e80a4b774b1127663dc21ea988b696
Segelzwerg <marcel.haas@hhu.de>
2020-05-22 23:24:25 +0200

    REFACTOR: shorten code for diagonal walking

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -54,10 +54,9 @@ namespace Character
                 // makes sure, that the total velocity is not higher while walking cross-ways
                 if (velocity.magnitude > 1.01)
                 {
-                    float ySaver = velocity.y;
-                    velocity.y = 0;
-                    velocity = velocity.normalized;
-                    velocity.y = ySaver;
+                    Vector2 planeVelocity = new Vector2(velocity.x, velocity.z).normalized;
+                    velocity.x = planeVelocity.x;
+                    velocity.z = planeVelocity.y;
                 }
 
                 // manages movement depending on being airborne or not
