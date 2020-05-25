f47b3bfa761f378174e98017502df5c0bfe776de
Segelzwerg <marcel.haas@hhu.de>
2020-05-25 20:40:24 +0200

    ADD: prey agent

---
+++ Assets/Scripts/Agents/AI/PreyAgent.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Agents.AI
+{
+    public class PreyAgent : BaseAgent
+    {
+        private void Update()
+        {
+            AddReward(0.1f);
+        }
+
+        public override void OnActionReceived(float[] vectorAction)
+        {
+            base.OnActionReceived(vectorAction);
+
+            float distance = CalculateDistanceToPredator();
+
+            SetReward((float) (distance / Math.Sqrt(2 * 11 * 11)));
+        }
+
+        private float CalculateDistanceToPredator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
